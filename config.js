ab47ba5d0d90e5adb39dfab28ff8fedec65b05a0
Manny <2204837+d3liriu5@users.noreply.github.com>
2023-08-22 21:28:26 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+const glob = require('glob');
+
+const tokensPath = `tokens/**/*`;
+const files = glob
+  .sync(tokensPath)
+  .map((filePath) => filePath.replace('tokens/', '').replace('.json', ''));
+
+module.exports = {
+  source: [`tokens/**/*`],
+  transform: {
+    testTransform: require('./transforms/testTransform'),
+  },
+  platforms: {
+    css: {
+      transformGroup: `css`,
+      buildPath: `build/`,
+      files: files.map((filePath) => {
+        return {
+          destination: `${filePath}.css`,
+          format: `css/variables`,
+          filter: (token) => token.filePath.includes(filePath),
+        };
+      }),
+    },
+  },
+};
